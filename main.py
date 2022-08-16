25dbf49e6066979d6d5255a928d5c17fcc89b76a
Aman_ <85976860+Aman6996@users.noreply.github.com>
2022-08-16 12:14:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
+# BEFORE SENDING TO REPLIT DO THESE CHANGES
+#Top of Main.py
+
+# from webserver import keep_alive
+
+#Bottom of Main.py
+
+# keep_alive()
+
+# TOKEN = os.environ.get("DISCORD_BOT_SECRET")
+
+# bot.run(TOKEN)
+
+
 import os
 import discord
 
 from dotenv import load_dotenv
 load_dotenv()
 
+
+
 from discord.ext import commands
 prefix = "$", "6."
-bot = commands.Bot(command_prefix=prefix, intents= discord.Intents.all())
+bot = commands.Bot(command_prefix=prefix, intents= discord.Intents.all(), case_insensitive=True)
+
+
+
+
 
 @bot.event
 async def on_message(message):
@@ -15,27 +35,40 @@ async def on_message(message):
     else:
         await bot.process_commands(message)
         if bot.user in message.mentions:
-                await message.channel.send("HEY")
-                       
+                await message.channel.send("Type `$help` to view all available commands")
+
+
 @bot.event
 async def on_ready():
     print('{0.user} is now online'.format(bot))
 
-@bot.command()
-async def hello(ctx):
+
+@bot.command(aliases=["hello"])
+async def hi(ctx):
     await ctx.reply("Hello!")
+
 
 # @bot.command()
 # async def image(ctx):
-#     await ctx.reply(file=discord.File("images/2022-07-07_14-54.png"))
+#     await ctx.send(file=discord.File("images/2022-07-07_14-54.png"))
+
 
 @bot.command()
-async def water(ctx):
-    await ctx.reply(file=discord.File("images/drink.png"))
+async def drink(ctx):
+    await ctx.reply(file=discord.File("images/water.png"))
+
 
 @bot.command()
 async def smile(ctx):
     await ctx.send("<:widesmile1:826114986338025543><:widesmile2:826114986446553089><:widesmile3:826114986543153173>")
+
+
+# @bot.command()
+# async def user_input(ctx, *, user_input):
+#     print(user_input)
+#     with open('input.txt', 'a') as f:
+#         f.write(user_input + "\n")
+
 
 @bot.command()
 async def uwu(ctx):
@@ -43,10 +76,12 @@ async def uwu(ctx):
     embed.set_image(url="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Stylized_uwu_emoticon.svg/1200px-Stylized_uwu_emoticon.svg.png")
     await ctx.reply(embed=embed)
 
+
 @bot.command()
-async def user_input(ctx, *, user_input):
-    print(user_input)
-    with open('input.txt', 'a') as f:
-        f.write(user_input + "\n")
+async def sayyourtoken(ctx):
+    await ctx.send("stfu")
+
+
+
 
 bot.run(os.getenv("TOKEN"))
